370275a10c7ffb77d6cb831894b165fccefab6ec
deepashreedev <deepashreedevarajan@gmail.com>
2025-07-07 23:50:28 +0530

    updated

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -15,7 +15,7 @@ const inWatchList = watchlist.some((m) => m.id == movie.id);
 <img
  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
 alt="{movie.title}"
-className='w-full  h-[30vh] md:h-[38vh] lg:h-[53vh] rounded-sm  p-2' />
+className='w-full  h-[30vh] md:h-[25vh] lg:h-[53vh] rounded-sm  p-2' />
 <h3 className='text-lg font-bold mt-2 px-3  '>{movie.title}</h3>
 <p className='text-sm text-gray-500 px-3 pt-1 pb-3 '>{movie.release_date}</p>
 <button className='absolute top-3 right-3 text-red-600 text-2xl font-bold ' onClick={() => toggleWatchlist(movie)}
